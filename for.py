725006c14ee896d0aad89ef037121271a4bc5c1c
daphnep <daphnep@cccgroupinc.com>
2022-09-08 15:26:44 -0500

    Initial Commit

---
+++ for.py
@@ -0,0 +1,20 @@
+# This is the basic syntax to write a for loop in Python
+
+# for <loop_variable> in <iterable>:
+#     <code>
+
+# The iterable can be a list, tuple, dictionary, string, the sequence returned by range, a file, or any other type of iterable in Python
+
+# We will start with range()
+
+
+for i in range(5):
+    print(i)
+
+
+# Output
+# 0
+# 1
+# 2
+# 3
+# 4
