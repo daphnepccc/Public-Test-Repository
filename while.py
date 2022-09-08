ecc62577c8de4a707fb89c0797fba1d9e7861757
daphnep <daphnep@cccgroupinc.com>
2022-09-08 13:04:43 -0500

    Initial Commit

---
+++ while.py
@@ -0,0 +1,15 @@
+# While loops are similar to for loops in that they let us repeat a block of code. The difference is that while loops run while a condition is True.
+
+# In a while loop, we define the condition, not the number of iterations. The loop stops when the condition is False.
+
+# This is the general syntax of a while loop:
+
+# while <condition>:
+#      <code>
+
+
+
+x = 6
+while x < 15:
+print (x)
+x += 1
